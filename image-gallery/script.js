5a32cb373071ceb78594efa99a42a3e624f74883
sadhagadeela <sadhagadeela@gmail.com>
2026-09-15 11:07:07 +0530

    Create script.js

---
+++ image-gallery/script.js
@@ -0,0 +1,7 @@
+function openModal(src){
+  document.getElementById('modal').style.display='flex';
+  document.getElementById('modalImg').src=src;
+}
+function closeModal(){
+  document.getElementById('modal').style.display='none';
+}
